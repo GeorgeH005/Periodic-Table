98fffc9ebc968fe5f76085d5b42bd98fa97d39b4
GeorgeH005 <haidos.george0@gmail.com>
2020-10-24 12:34:04 +0300

    Imported Vasilis Code

--- Chemistry Bill.py
+++ Chemistry Bill.py
@@ -9,6 +9,18 @@ def get_num(element):
 		raise SystemExit
 	return num
 	
+def get_element(num):
+	nums={1:'H',2:'He',3:'Li',4:'Be',5:'B',6:'C',7:'N',8:'O',9:'F',10:'Ne',11:'Na',12:'Mg',13:'Al',14:'Si',15:'P',16:'S',17:'Cl',18:'Ar',19:'K',20:'Ca',31:'Ga',32:'Ge',33:'As',34:'Se',35:'Br',36:'Kr',37:'Rb',38:'Sr',49:'In',50:'Sn',51:'Sb',52:'Te',53:'I',54:'Xe',55:'Cs',56:'Ba',81:'Tl',82:'Pb',83:'Bi',84:'Po',85:'At',86:'Rn',87:'Fr',88:'Ra',113:'Nh',114:'Fl',115:'Mc',116:'Lv',117:'Ts',118:'Og'}
+	if num in nums:
+#		num=int(num)
+		element=nums[num]
+		print('  Element:', element)
+	else:
+		print("\033[1;31;40m")
+		print("Number Not Supported!")
+		raise SystemExit
+	return num
+
 #get period line			
 def get_period(num):
 	if num<=2:
